c824f89de54c4f8ea0cafd2bc310e8f8d84bcd24
Viktor Stefanov <victorystefanov@gmail.com>
2024-09-08 12:51:03 +0300

    nav bar items constant

---
+++ client/src/constants/navBarItems.ts
@@ -0,0 +1,18 @@
+export const NavBarItem = [
+    {
+        title: 'Home',
+        route: '/'
+    },
+    {
+        title: 'Login',
+        route: '/auth/login'
+    },
+    {
+        title: 'Register',
+        route: '/auth/register'
+    },
+    {
+        title: 'Logout',
+        route: '/auth/logout'
+    }
+]
